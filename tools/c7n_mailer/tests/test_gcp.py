5a19590a0eedc6b3c49bbdfacb3b5f1e6c6dffa0
Brent Clements <brent.clements@gmail.com>
2019-05-15 14:39:33 -0500

    Corrected test_gcp.py to fix codecov failure

--- tools/c7n_mailer/tests/test_gcp.py
+++ tools/c7n_mailer/tests/test_gcp.py
@@ -16,7 +16,7 @@ import base64
 import json
 import unittest
 import zlib
-from common import logger, MAILER_CONFIG_1, GCP_MESSAGE
+from common import logger, MAILER_CONFIG_1, GCP_MESSAGE, GCP_MESSAGES
 from c7n_mailer.gcp.gcp_pubsub_processor import MailerGcpPubSubProcessor
 from mock import MagicMock, patch
 
@@ -29,13 +29,24 @@ class GcpTest(unittest.TestCase):
             zlib.compress(GCP_MESSAGE.encode('utf8')))
         self.loaded_message = json.loads(GCP_MESSAGE)
 
-    @patch.object(MailerGcpPubSubProcessor, 'process_message')
-    def test_process_pubsub_message_success(self, mock_process):
-        processor = MailerGcpPubSubProcessor(MAILER_CONFIG_1, logger)
-        self.assertTrue(processor.process_message(self.compressed_message))
-
     @patch.object(MailerGcpPubSubProcessor, 'receive_messages')
-    def test_run(self, mock_receive):
+    @patch.object(MailerGcpPubSubProcessor, 'process_message')
+    @patch.object(MailerGcpPubSubProcessor, 'ack_messages')
+    def test_process_pubsub_message_success(self, mock_ack, mock_process, mock_receive):
+        mock_process.return_value = True
+        mock_ack.return_value = True
+        mock_receive.return_value = GCP_MESSAGES
+        processor = MailerGcpPubSubProcessor(MAILER_CONFIG_1, logger)
+        self.assertEqual(processor.receive_messages(), GCP_MESSAGES)
+        mock_receive.assert_called()
+        self.assertTrue(processor.process_message(self.compressed_message))
+        processor.ack_messages('2019-05-13T18:31:17.926Z')
+        mock_ack.assert_called_once_with('2019-05-13T18:31:17.926Z')
+
+    @patch.object(MailerGcpPubSubProcessor, 'run')
+    def test_run(self, mock_run):
+        mock_run.return_value = None
         processor = MailerGcpPubSubProcessor(MAILER_CONFIG_1, logger)
         processor.run()
-        self.assertEqual(1, mock_receive.call_count)
+        self.assertEqual(1, mock_run.call_count)
+        mock_run.assert_called()
