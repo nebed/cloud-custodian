8538d72badc6b640bb8e2d1a97ea4e2c81f66ec8
Brent Clements <brent.clements@gmail.com>
2019-05-19 17:14:35 -0500

    Added More tests for gcp mailer code

--- tools/c7n_mailer/tests/test_gcp.py
+++ tools/c7n_mailer/tests/test_gcp.py
@@ -12,12 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 import json
 import unittest
 from common import logger, MAILER_CONFIG_1, GCP_MESSAGE, GCP_MESSAGES
 from c7n_mailer.gcp.gcp_pubsub_processor import MailerGcpPubSubProcessor
 from c7n_mailer.email_delivery import EmailDelivery
 from mock import patch
+import c7n_mailer.cli
 
 
 class GcpTest(unittest.TestCase):
@@ -32,9 +34,29 @@ class GcpTest(unittest.TestCase):
         processor = MailerGcpPubSubProcessor(MAILER_CONFIG_1, logger)
         self.assertIsNone(processor.process_message(GCP_MESSAGES['receivedMessages'][0]))
 
-    @patch.object(MailerGcpPubSubProcessor, 'ack_messages')
+    def test_receive(self):
+        processor = MailerGcpPubSubProcessor(MAILER_CONFIG_1, logger)
+        messages = processor.receive_messages()
+        self.assertEqual(messages, {})
+
+    def test_ack(self):
+        processor = MailerGcpPubSubProcessor(MAILER_CONFIG_1, logger)
+        self.assertEqual(processor.ack_messages('2019-05-13T18:31:17.926Z'), {})
+
     @patch.object(MailerGcpPubSubProcessor, 'receive_messages')
     def test_run(self, mock_receive):
         mock_receive.return_value = []
         processor = MailerGcpPubSubProcessor(MAILER_CONFIG_1, logger)
         processor.run()
+
+    def test_is_gcp_cloud(self):
+        self.assertTrue(c7n_mailer.cli.is_gcp_cloud(MAILER_CONFIG_1))
+
+    @patch('common.logger.info')
+    @patch.object(MailerGcpPubSubProcessor, 'receive_messages')
+    def test_processor_run_logging(self, mock_receive, mock_log):
+        mock_receive.return_value = []
+        processor = MailerGcpPubSubProcessor(MAILER_CONFIG_1, logger)
+        processor.run()
+        mock_log.assert_called_with('No messages left in the gcp topic subscription,'
+                                    ' now exiting c7n_mailer.')
